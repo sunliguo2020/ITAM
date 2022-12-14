fac5a86712352eac2360e803c123e39928d36e66
sunliguo <sunliguo2006@qq.com>
2022-12-14 14:48:10 +0800

    检查mac的合法性

--- app1/utils/form.py
+++ app1/utils/form.py
@@ -4,8 +4,11 @@
 @contact: QQ376440229
 @Created on: 2022/12/13 11:54
 """
+import re
 
 from django import forms
+from django.core.exceptions import ValidationError
+
 from app1 import models
 from .bootstrap import BootStrapModelForm
 
@@ -19,6 +22,26 @@ class ComputerModelForm(BootStrapModelForm):
                 'unique': '此序列号已经存在!',
             }
         }
+
+    def clean_mac_addr(self):
+        """
+        检查mac地址的合法性
+        :return:
+        """
+        mac_addr = self.cleaned_data.get('mac_addr')
+        # valid = re.compile(r'''
+        #                     (^([0-9A-F]{1,2}[-]){5}([0-9A-f]{1,2})$
+        #                     |^([0-9A-F]{1,2}[:]){5}([0-9A-f]{1,2})$
+        #                     |^([0-9A-F]{1,2}[.]){5}([0-9A-f]{1,2})$)
+        #                     ''',
+        #                    re.VERBOSE | re.IGNORECASE)
+        valid = re.compile(r'''
+                            (^([0-9A-F]{4}[-])([0-9A-f]{4}[-])([0-9A-F]{4})$)
+                            ''',
+                           re.VERBOSE | re.IGNORECASE)
+        if not valid.match(mac_addr):
+            raise ValidationError('注意MAC地址的格式XXXX-XXXX-XXXX')
+        return mac_addr
 
 
 class UserModelForm(BootStrapModelForm):
