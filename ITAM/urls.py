3dd7b59307fd927e6f8d835c571f1d147a41acef
sunliguo <sunliguo2006@qq.com>
2022-12-15 22:33:14 +0800

    图片url更正

--- ITAM/urls.py
+++ ITAM/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
+
 from app1 import views
 
 app_name = 'app1'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 显示图片
+    # re_path(r'meida/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}, name='media'),
+    re_path('media/(?P<path>.*)', serve, {"document_root": settings.MEDIA_ROOT}),
     # 电脑显示页面
     path('computer/list/', views.computer_list),
     # 电脑编辑
