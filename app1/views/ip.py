a1242bc751be7a635bfc4e69ab011a7f2f986b4e
sunliguo <sunliguo2006@qq.com>
2022-12-16 22:45:09 +0800

    分页显示+批量上传（存在问题：导入时间需要时区的错误提示，不过能导入。重复值问题。数据量大的问题）

--- app1/views/ip.py
+++ app1/views/ip.py
@@ -4,15 +4,22 @@
 @contact: QQ376440229
 @Created on: 2022-12-16 18:32
 """
-from django.shortcuts import render, redirect
+import csv
+
+from django.shortcuts import render, redirect, HttpResponse
+from openpyxl import load_workbook
+
 from app1 import models
 from app1.utils.form import IpModelForm
+from app1.utils.pageination import Pagination
 
 
 def ip_list(request):
     queryset = models.IpAddr.objects.all()
+    page_object = Pagination(request,queryset)
     context = {
-        'queryset': queryset,
+        'queryset': page_object.page_queryset,
+        'page_string':page_object.html()
     }
     return render(request, 'ip_list.html', context)
 
@@ -53,6 +60,32 @@ def ip_edit(request, nid):
     return render(request, 'change.html', context)
 
 
-def ip_delete(request,nid):
+def ip_delete(request, nid):
     models.IpAddr.objects.filter(id=nid).delete()
+    return redirect('/ip/list/')
+
+
+def ip_multi(request):
+    """
+    ip arp 地址对应关系导入
+    :param request:
+    :return:
+    """
+    # 是否上传空文件
+    file_obj = request.FILES.get('csv_file', None)
+    if not file_obj:
+        return redirect('/ip/list/')
+
+    wb = load_workbook(file_obj)
+    sheet = wb.worksheets[0]
+    for row in sheet.iter_rows(min_row=1):
+        data_dict = {
+            'ip_addr': row[0].value,
+            'mac_addr': row[1].value,
+            'interface': row[2].value,
+            'cap_datetime': row[3].value,
+        }
+
+        models.IpAddr.objects.create(**data_dict)
+
     return redirect('/ip/list/')
