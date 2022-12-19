1cd56ebcdcf82edc93f0c7492e962c36d3f91a0b
sunliguo <sunliguo2006@qq.com>
2022-12-19 18:33:27 +0800

    数据库又改回mysql

--- ITAM/settings.py
+++ ITAM/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'ITAM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'itam',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
