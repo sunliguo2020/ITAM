906d4c2282557e71d6847225a248e8c38565d7b4
sunliguo <sunliguo2006@qq.com>
2022-12-19 12:36:44 +0800

    静态目录

--- ITAM/settings.py
+++ ITAM/settings.py
@@ -114,6 +114,7 @@ TIME_ZONE = 'Asia/Shanghai'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
