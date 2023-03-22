9240ed71c7b37da87555d488cc325efe5db98181
sunliguo <sunliguo2006@qq.com>
2023-03-22 16:19:00 +0800

    登录验证码字符改为4个

--- app1/utils/code.py
+++ app1/utils/code.py
@@ -9,7 +9,7 @@ import random
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 
-def check_code(width=120, height=30, char_length=5, font_file='font.ttf', font_size=28):
+def check_code(width=120, height=30, char_length=4, font_file='font.ttf', font_size=28):
     code = []
     img = Image.new(mode='RGB', size=(width, height), color=(255, 255, 255))
     draw = ImageDraw.Draw(img, mode='RGB')
